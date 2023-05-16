e1b69520650bff83b17abd46cc9a909297115b36
abirukov2008 <abirukov2008@gmail.com>
2023-05-16 19:10:05 +0300

    app

---
+++ beaver_app/app.py
@@ -0,0 +1,20 @@
+import logging
+
+from flask import Flask
+from flask_migrate import Migrate
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from beaver_app.db import db
+from beaver_app.config import get_config, get_connection_dsn
+logging.basicConfig(level=logging.INFO)
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.update(get_config())
+    app.engine = create_engine(get_connection_dsn(app.config), echo=True, query_cache_size=0)
+    app.session = scoped_session(sessionmaker(app.engine))
+    # db.init_app(app)
+    migrate = Migrate(app, db)
+    return app
