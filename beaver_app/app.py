a6ccaeab5cfabbef143976b46e8a69b8cbada49b
abirukov2008 <abirukov2008@gmail.com>
2023-05-24 22:47:54 +0300

    ga, sentry

--- beaver_app/app.py
+++ beaver_app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
-from logging.config import dictConfig
+import sentry_sdk
 
+from flask import Flask
 from flask_smorest import Api
+from logging.config import dictConfig
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from beaver_app.blueprints.category.routes import category_blueprint
 from beaver_app.blueprints.product.routes import product_blueprint
@@ -9,6 +11,18 @@ from beaver_app.config import get_config
 
 
 def create_app() -> Flask:
+
+    sentry_sdk.init(
+        dsn="https://46ba4ee456734c1e88da5ba1b320ad18@o1164730.ingest.sentry.io/4505240675942400",
+        integrations=[
+            FlaskIntegration(),
+        ],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
 
     dictConfig({
         'version': 1,
