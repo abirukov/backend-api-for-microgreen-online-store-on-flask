2c0e713f7fa49d9786000710c77ad8fb05da1d5f
abirukov2008 <abirukov2008@gmail.com>
2023-06-16 10:16:02 +0300

    правки

--- beaver_app/blueprints/basket/utils.py
+++ beaver_app/blueprints/basket/utils.py
@@ -1,5 +1,6 @@
 from beaver_app.blueprints.basket.enums import RecordTypeUpdateBasket
 from beaver_app.blueprints.basket.models import Basket, BasketProduct
+from beaver_app.blueprints.product.classes.product_update_row import ProductUpdateRow
 from beaver_app.blueprints.product.models import Product
 from beaver_app.db.db_utils import save
 from beaver_app.db.db import db_session
@@ -15,39 +16,40 @@ def handle_record_type(record_type: RecordTypeUpdateBasket, basket: Basket, prod
     if record_type.value not in products_data:
         return
     for product_row in products_data[record_type.value]:
+        product_update_row = ProductUpdateRow(product_row['product_id'], product_row['quantity'])
         if record_type == RecordTypeUpdateBasket.ADD:
-            add_product(basket, product_row)
+            add_product(basket, product_update_row)
         else:
-            remove_product(basket, product_row)
+            remove_product(basket, product_update_row)
 
 
-def add_product(basket: Basket, product_row: dict[str, str | float]) -> None:
+def add_product(basket: Basket, product_row: ProductUpdateRow) -> None:
     product_row_in_basket = list(filter(
-        lambda product_r: str(product_r.product_id) == product_row['product_id'],
+        lambda product_r: str(product_r.product_id) == product_row.product_id,
         basket.basket_products,
     ))
     if not product_row_in_basket:
-        product = Product.query.filter_by(id=product_row['product_id']).first()
+        product = Product.query.filter_by(id=product_row.product_id).first()
         if product is None:
-            raise Exception(f'not found product with id = {product_row["product_id"]}')
+            raise Exception(f'not found product with id = {product_row.product_id}')
         save(BasketProduct(
             basket_id=basket.id,
             product_id=product.id,
-            quantity=product_row['quantity'],
+            quantity=product_row.quantity,
         ))
     else:
-        product_row_in_basket[0].quantity += float(product_row['quantity'])
+        product_row_in_basket[0].quantity += float(product_row.quantity)
 
 
-def remove_product(basket: Basket, product_row: dict[str, str | float]) -> None:
+def remove_product(basket: Basket, product_row: ProductUpdateRow) -> None:
     product_row_in_basket = list(filter(
-        lambda product_r: str(product_r.product_id) == product_row['product_id'],
+        lambda product_r: str(product_r.product_id) == product_row.product_id,
         basket.basket_products,
     ))
     if product_row_in_basket is None:
-        raise f'not found in basket(id={basket.id}) with product (id = {product_row["product_id"]})'
-    elif product_row_in_basket[0].quantity > float(product_row['quantity']):
-        product_row_in_basket[0].quantity -= float(product_row['quantity'])
+        raise Exception(f'not found in basket(id={basket.id}) with product (id = {product_row.product_id})')
+    elif product_row_in_basket[0].quantity > float(product_row.quantity):
+        product_row_in_basket[0].quantity -= float(product_row.quantity)
     else:
-        product = Product.query.filter_by(id=product_row['product_id']).first()
+        product = Product.query.filter_by(id=product_row.product_id).first()
         basket.products.remove(product)
