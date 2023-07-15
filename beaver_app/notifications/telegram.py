e51fd95c8f96be8dc25c1437d03e06e513f2c879
abirukov2008 <abirukov2008@gmail.com>
2023-07-15 10:19:53 +0300

    notificate

--- beaver_app/notifications/telegram.py
+++ beaver_app/notifications/telegram.py
@@ -5,19 +5,22 @@ from beaver_app.blueprints.user.models import User
 
 
 def notify_about_order(user: User, order: Order) -> None:
-    text = prepare_text(order)
-    send_message(user.tg_id, text)
+    if user.tg_id is not None:
+        text = prepare_text(order)
+        send_message(str(user.tg_id), text)
+    return
 
 
 def prepare_text(order: Order) -> str:
-    return f'Ваш заказ № {order.id} на сумму {order.total} принят. И будет доставлен по адресу {order.address}'
+    return f'Ваш заказ № {order.id} на сумму {order.total} рублей принят. И будет доставлен по адресу {order.address}'
 
 
 def send_message(chat_id: str, text: str) -> None:
     requests.post(
-        'https://bot.green-beaver.ru',
+        'https://bot.green-beaver.ru/send_message',
         json={
             'chat_id': chat_id,
             'text': text,
         },
     )
+    return
